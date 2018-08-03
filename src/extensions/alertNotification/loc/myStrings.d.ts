6a9459dd9730ea1f0a94e321aab6996d7545b5c4
Lodhia <klodhia@sffirecu.org>
2018-08-03 11:49:42 -0700

    Adding all files to git. Working copy with CDN.

---
+++ src/extensions/alertNotification/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAlertNotificationApplicationCustomizerStrings {
+  MoreInformation: string;
+}
+
+declare module 'AlertNotificationApplicationCustomizerStrings' {
+  const strings: IAlertNotfticationApplicationCustomizerStrings;
+  export = strings;
+}
